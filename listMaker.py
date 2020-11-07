c4f5b1ce781b3b8579df7cad4f755b6d44dffa82
INDA010010 <74112294+INDA010010@users.noreply.github.com>
2020-11-07 19:28:55 +0100

    Add files via upload

---
+++ listMaker.py
@@ -0,0 +1,12 @@
+num = int(input("Enter start number: "))
+result = int(input("Enter end number: "))
+while num <= result:
+    if num == 1:
+        print("[", str(num), ", ")
+        num += 1
+    if num != 1 and num <= result:
+        print(str(num),", ")
+        num += 1
+    if num == result:
+        print(str(num), "]")
+
