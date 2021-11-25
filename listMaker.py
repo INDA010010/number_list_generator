1339f480525f6441ef0d1f80d36a85c15f138828
INDA010010 <74112294+INDA010010@users.noreply.github.com>
2021-11-25 12:24:01 +0100

    Update listMaker.py

--- listMaker.py
+++ listMaker.py
@@ -1,15 +1,7 @@
 num = int(input("Enter start number: "))
-num2 = num
 end = int(input("Enter end number: "))
-output = ""
+output = []
 while num <= end:
-    if num == num2:
-        output += "["+str(num)+", "
-        num += 1
-    if num != num2 and num != end:
-        output += str(num)+", "
-        num += 1
-    if num == end:
-        output += str(num)+"]"
-        break
+   output.append(num)
+   num += 1 
 print(output)
